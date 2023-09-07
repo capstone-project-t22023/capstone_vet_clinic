09f0a563d656ac1e7ad45902cc7d3af1725a5d1d
KaynethCalica <kaynethcalica@gmail.com>
2023-09-07 12:44:20 +1000

    remove header and footer from login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import {Container} from '@mui/material';
-import Header from "../components/Header"
-import Footer from "../components/Footer";
 import LoginForm from '../components/authorization/LoginForm'
 
 export default function Login() {
@@ -11,7 +9,6 @@ export default function Login() {
 
     return (
         <div>
-            <Header/>
             <Container maxWidth="md" sx={{mt: 5, mb: 5, pt:3, pb:10, border: "5px solid #f5effb", borderColor: "grey.100"}}>
                     <LoginForm/>
             </Container>
@@ -19,7 +16,6 @@ export default function Login() {
             <p>Pet owner: abundantasparagus -> abundantasparagus_2023</p>
             <p>Doctor: teemingbroth -> teemingbroth_2023</p>
             <p>admin: pawsome_admin -> pawsome_admin2023</p>
-            <Footer/>
         </div>
     )
 }
