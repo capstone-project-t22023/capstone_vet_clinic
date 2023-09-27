4e6a9fc1c615b250518f07c6af80a3887a4ac9c8
KaynethCalica <kaynethcalica@gmail.com>
2023-09-27 21:38:17 +1000

    cucumber modifications

--- e2e-tests/lib/step_definitions.js
+++
@@ -1,8 +0,0 @@
-const { Given, When, Then, After } = require("@cucumber/cucumber");
-const assert = require("assert/strict");
-const { By, until } = require("selenium-webdriver");
-
-After(async function () {
-    await this.driver.quit();
-  });
-  
