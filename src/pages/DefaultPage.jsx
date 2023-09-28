3c99e1e766e81b1e2064d1c369fa465e0f22c0c1
Malgorzata Mika <malgorzata.mika87@gmail.com>
2023-09-28 13:48:30 +1000

    404 modified

--- src/pages/DefaultPage.jsx
+++ src/pages/DefaultPage.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 export default function StaticLanding() {
 
     useEffect(() => {
-        window.location.replace("http://localhost:3000/public/404/Patch/index.html");
+        window.location.replace("http://localhost:3000/404/Patch/index.html");
     }, []);
     return (
         <div></div>
